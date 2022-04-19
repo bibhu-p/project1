085bfa1554bfd384440dcb2a4e181f770f99ced8
Bibhu Prasad <bibhup.behera@nettantra.net>
2022-04-19 18:22:08 +0530

    Started

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import '../App.css'
 
 export class Home extends Component {
   render() {
     return (
-      <div>Home Page</div>
+      <div class="home-body">
+        
+      </div>
     )
   }
 }
